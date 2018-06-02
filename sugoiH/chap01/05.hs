fadfe3876ae6e00234bb28b8c2c188ed9ab5ab43
wheson <wheson.hxs@gmail.com>
2018-06-02 12:04:14 +0900

    chap01終了

---
+++ sugoiH/chap01/05.hs
@@ -0,0 +1,6 @@
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [ 1 | _ <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+removeNonLowercase st = [ c | c <- st, c `elem` ['a'..'z']]
